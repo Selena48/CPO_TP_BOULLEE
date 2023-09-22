76d68fa3de6d51aaf7eb5d5c2d739c4a5329ac41
Sélèna Boullée <sboullee@kalya-sante.com>
2023-09-22 13:24:06 +0200

    (create): create new project

---
+++ TP0_hello_world/src/tp0_hello_world/TP0_hello_world.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_hello_world;
+
+/**
+ *
+ * @author NB-X02
+ */
+public class TP0_hello_world {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
