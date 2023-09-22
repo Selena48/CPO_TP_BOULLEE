ca82de17d6a3eeb6c10f9ab81e357fa62ecfc822
Sélèna Boullée <sboullee@kalya-sante.com>
2023-09-22 14:50:16 +0200

    (feat) : add BouteilleBiere class and main program in TP2_Bieres.java

---
+++ TP2_Bieres/src/tp2_bieres/BouteilleBiere.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres;
+
+/**
+ *
+ * @author NB-X02
+ */
+public class BouteilleBiere {
+    
+    //Constructor
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+    nom = unNom;
+    degreAlcool = unDegre;
+    brasserie = uneBrasserie;
+    ouverte = false;
+}
+    
+    /*Attributs*/
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;   
+    
+    /*Méthodes*/
+    public void lireEtiquette(){
+        System.out.println("Bouteille de " + nom +" (" + degreAlcool +" degres) \nBrasserie : " + brasserie ) ;
+    }
+    public boolean Decapsuler(){
+        if (ouverte){
+           System.out.println("Erreur: biere déjà ouverte");
+           return false;
+        }else{
+            ouverte = true;
+            return true;
+        }
+    }
+    @Override
+    public String toString() {
+    String chaine_a_retourner;
+    chaine_a_retourner = nom + " (" + degreAlcool + " degrés)Ouverte ? ";
+    if (ouverte == true ) chaine_a_retourner += "oui" ;
+    else chaine_a_retourner += "non" ;
+    return chaine_a_retourner ;
+    }
+
+}
--- TP2_Bieres/src/tp2_bieres/TP2_Bieres.java
+++ TP2_Bieres/src/tp2_bieres/TP2_Bieres.java
@@ -15,6 +15,36 @@ public class TP2_Bieres {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+       //première bière
+       /*BouteilleBiere uneBiere = new BouteilleBiere() ;
+       uneBiere.nom ="Cuvée des trolls";
+       uneBiere.degreAlcool= 7.0;
+       uneBiere.brasserie="Dubuisson";
+       uneBiere.lireEtiquette();*/
+      BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",7.0,"Dubuisson") ;
+      //uneBiere.lireEtiquette();
+       
+       //Deuxième bière
+       /*BouteilleBiere deuxiemeBiere = new BouteilleBiere();
+       deuxiemeBiere.nom="Leffe";
+       deuxiemeBiere.degreAlcool=6.6;
+       deuxiemeBiere.brasserie="Abbaye de Leffe";*/
+       
+      BouteilleBiere autreBiere = new BouteilleBiere("Leffe",6.6,"Abaye de Leffe") ;
+      //autreBiere.lireEtiquette();
+      
+      BouteilleBiere troisBiere = new BouteilleBiere("Trois",6.0,"Abaye de Trois") ;
+      //troisBiere.lireEtiquette();
+      
+      BouteilleBiere quatreBiere = new BouteilleBiere("Leffre",6.8,"Abaye de Leffre") ;
+      //quatreBiere.lireEtiquette();
+      
+      BouteilleBiere cinqBiere = new BouteilleBiere("Left",6.2,"Abaye de Left") ;
+      //cinqBiere.lireEtiquette();
+      cinqBiere.Decapsuler();
+      cinqBiere.toString();
+      System.out.println(cinqBiere) ;
     }
     
 }
