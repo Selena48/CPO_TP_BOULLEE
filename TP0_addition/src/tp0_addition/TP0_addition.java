7d45f43172dff5cac70a3541648d4a3699f71808
Sélèna Boullée <sboullee@kalya-sante.com>
2023-09-24 14:13:15 +0200

    (feat): calculating the sum of integers from a chosen number

--- TP0_addition/src/tp0_addition/TP0_addition.java
+++ TP0_addition/src/tp0_addition/TP0_addition.java
@@ -4,6 +4,8 @@
  */
 package tp0_addition;
 
+import java.util.Scanner;
+
 /**
  *
  * @author NB-X02
@@ -15,6 +17,23 @@ public class TP0_addition {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        int nb; // nombre d'entiers à additionner
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrer le nombre :");
+        nb=sc.nextInt();
+        int result; // resultat
+        int ind; //indice
+        // initialisation
+
+        result=0;
+        // addition des nb premiers entiers
+        ind=1;
+        while (ind<= nb) {
+         result=result+ind;
+         ind++;
+        }
+        // affichage du resultat
+        System.out.println("la somme des "+ nb + " entiers est:"+result); 
+    } 
     
 }
