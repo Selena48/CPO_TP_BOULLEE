33e0e2c011cbc0fb6f17440565115819b6dba019
Sélèna Boullée <sboullee@kalya-sante.com>
2023-10-16 16:24:25 +0200

    (create):create new project

---
+++ TP_JFRAME_video2/src/tp_jframe_video2/TP_JFRAME_video2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp_jframe_video2;
+
+/**
+ *
+ * @author NB-X02
+ */
+public class TP_JFRAME_video2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
