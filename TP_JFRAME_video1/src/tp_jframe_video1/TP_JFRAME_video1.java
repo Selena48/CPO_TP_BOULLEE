032b70974199c459611e304e83c882dba9b29d72
Sélèna Boullée <sboullee@kalya-sante.com>
2023-10-16 16:22:36 +0200

    (feat): add button with label event

--- TP_JFRAME_video1/src/tp_jframe_video1/TP_JFRAME_video1.java
+++ TP_JFRAME_video1/src/tp_jframe_video1/TP_JFRAME_video1.java
@@ -13,8 +13,8 @@ public class TP_JFRAME_video1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    /**public static void main(String[] args) {
         // TODO code application logic here
-    }
+    }*/
     
 }
