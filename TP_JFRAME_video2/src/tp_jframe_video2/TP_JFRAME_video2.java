2b567258aff2cbc3d2a90b3220fdcf2f09ceef03
Sélèna Boullée <sboullee@kalya-sante.com>
2023-10-17 09:16:22 +0200

    (feat):add jFrame with a button that counts the number of clicks and stops at 4

--- TP_JFRAME_video2/src/tp_jframe_video2/TP_JFRAME_video2.java
+++ TP_JFRAME_video2/src/tp_jframe_video2/TP_JFRAME_video2.java
@@ -13,8 +13,6 @@ public class TP_JFRAME_video2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
     
 }
